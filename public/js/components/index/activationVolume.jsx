f13889772ff8178a87fb989f9bdeb8947e822328
xianwganchun <261037213@qq.com>
2016-03-31 16:15:53 +0800

    导航加上拖拽改变位置功能,

--- public/js/components/index/activationVolume.jsx
+++ public/js/components/index/activationVolume.jsx
@@ -21,28 +21,28 @@ const ActivationVolume = React.createClass({
                 <div className="AVInner">
                 	<h4>今日激活量</h4>
                 	<p className="AVnum">{propsData.activation_today}</p>
-                	<p className="AVgrowth"><i className="iconfont icon-xiangshang"></i> 6</p>
+                	<p className="AVgrowth"><i className="iconfont icon-xiangshang"></i>{propsData.activation_today}</p>
                 </div>
             </Col>
             <Col span="6">
                 <div className="AVInner">
                 	<h4>今日活跃数</h4>
-                	<p className="AVnum">{propsData.active_today}</p>
-                	<p className="AVgrowth"><i className="iconfont icon-xiangshang"></i>20</p>
+                	<p className="AVnum">{propsData.startup_today}</p>
+                	<p className="AVgrowth"><i className="iconfont icon-xiangshang"></i>{propsData.startup_difference}</p>
                 </div>
             </Col>
             <Col span="6">
                 <div className="AVInner">
                 	<h4>今日点击数</h4>
                 	<p className="AVnum">{propsData.hit_today}</p>
-                	<p className="AVgrowth"><i className="iconfont icon-xiangshang"></i>168</p>
+                	<p className="AVgrowth"><i className="iconfont icon-xiangshang"></i>{propsData.hit_difference}</p>
                 </div>
             </Col>
             <Col span="6">
                 <div>
                 	<h4>总激活量</h4>
-                	<p className="AVnum">{propsData.activation}</p>
-                	<p className="AVgrowth"><i className="iconfont icon-xiangshang"></i>20</p>
+                	<p className="AVnum">{propsData.activation_all}</p>
+                	<p className="AVgrowth"><i className="iconfont icon-xiangshang"></i>{propsData.activation_all_difference}</p>
                 </div>
             </Col>
           </Row>
--- public/js/views/clientManagement/clientManagementMenu.jsx
+++ public/js/views/clientManagement/clientManagementMenu.jsx
@@ -6,6 +6,7 @@ import '../../../css/clientManagement.less'
 import { Menu, Icon,Button,Tabs,Alert,Table,Row,Col,message,Modal} from 'antd'
 import 'ztree'
 import '../../plug/ztree/css/zTreeStyle/zTreeStyle.css'
+/*import '../../plug/ztree/css/awesomeStyle/awesome.css'*/
 import CONFIG from '../../config/API'
 import MenuType from './menu/menuType'
 import ManagementMenuNew from './MenuMangement/ManagementMenuNew';
@@ -125,6 +126,26 @@ var menuprops={
         "app_navigate_style": null
 }
 
+
+/*view: {
+                addHoverDom: addHoverDom,
+                removeHoverDom: removeHoverDom,
+                selectedMulti: false
+            },
+            check: {
+                enable: true
+                ,chkStyle: 'radio'
+                ,radioType: "level"
+            },
+            data: {
+                simpleData: {
+                    enable: true
+                }
+            },
+            edit: {
+                enable: true
+            }*/
+
 const ClientManagementMenu = React.createClass({
   getInitialState() {
     return {
@@ -134,11 +155,21 @@ const ClientManagementMenu = React.createClass({
       menuType : <MenuType fun={this.menuTypeFun}/>,
       setting : {
                   view: {
-                    dblClickExpand: false
+                    dblClickExpand: false,
+                  },
+                  edit: {
+                      enable: true,
+                      showRemoveBtn: false,
+                      showRenameBtn: false,
+                      drag: {
+                        autoExpandTrigger: true
+                      }
                   },
                   callback: {
                     onRightClick: this.OnRightClick,
-                    onClick: this.OnClick
+                    onClick: this.OnClick,
+                    beforeDrag: this.zTreeBeforeDrag,
+                    onDrop: this.zTreeOnDrop
                   }
                 },
       navigateData : '',
@@ -150,6 +181,40 @@ const ClientManagementMenu = React.createClass({
   hideRMenu() {
     if (rMenu) rMenu.css({"visibility": "hidden"});
     $("body").unbind("mousedown", this.onBodyMouseDown);
+  },
+  zTreeBeforeDrag(treeId, treeNodes){
+    
+  },
+  zTreeOnDrop(event, treeId, treeNodes, targetNode, moveType) {
+
+    var treeObj = $.fn.zTree.getZTreeObj(treeId);
+    var nodes = treeObj.getNodes();
+
+    $.post(CONFIG.HOSTNAME+'/navigate/relation',{'relation' : nodes},function(ajaxdata){
+        /*console.log(ajaxdata);*/
+        let data = this.state.data;
+        ajaxdata = JSON.parse(ajaxdata);
+        if(ajaxdata.state){
+          Modal.success({
+            title: '成功信息',
+            content: `恭喜您!导航位置关系改变成功`
+          });
+        }else{
+          Modal.error({
+            title: '失败提醒',
+            content: `导航位置关系改变失败,请重试!`
+          });
+        }  
+    }.bind(this));
+
+    /*console.log('====================treeNodes');
+    console.log(treeNodes);
+    console.log('====================targetNode');
+    console.log(targetNode);
+    console.log('====================moveType');
+    console.log(moveType)*/
+
+
   },
   OnRightClick(event, treeId, treeNode){
     curTreeNode = treeNode;
@@ -164,6 +229,22 @@ const ClientManagementMenu = React.createClass({
       this.showRMenu("node", position.left+50, position.top);
     }
   },
+  addHoverDom(treeId, treeNode) {
+      var sObj = $("#" + treeNode.tId + "_span");
+      if (treeNode.editNameFlag || $("#addBtn_"+treeNode.tId).length>0) return;
+      var addStr = "<span class='button add' id='addBtn_" + treeNode.tId
+          + "' title='add node' onfocus='this.blur();'></span>";
+      sObj.after(addStr);
+      var btn = $("#addBtn_"+treeNode.tId);
+      if (btn) btn.bind("click", function(){
+          var zTree = $.fn.zTree.getZTreeObj("treeDemo");
+          zTree.addNodes(treeNode, {id:(100 + newCount), pId:treeNode.id, name:"new node" + (1)});
+          return false;
+      });
+  },
+   removeHoverDom(treeId, treeNode) {
+        $("#addBtn_"+treeNode.tId).unbind().remove();
+    },
   showRMenu(type, x, y) {
     /* message.info('这是一条普通的提醒');
     console.log('#######################');
